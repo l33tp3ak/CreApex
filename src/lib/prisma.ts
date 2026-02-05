07b4795e6a2f41a50f408716e7be9cc86069af6a
l33tp3ak <m431f2@gmail.com>
2026-02-04 19:00:06 -0500

    First commit, creation of a basic API structure using Next.JS

---
+++ src/lib/prisma.ts
@@ -0,0 +1,25 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/TypeScriptDataObjectTemplate.ts to edit this template
+ */
+
+import {PrismaNeon} from '@prisma/adapter-neon';
+import {PrismaClient} from '../generated/prisma/client';
+
+// The following is the same as writing import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Créer le client Neon adapter
+const adapter = new PrismaNeon({
+	connectionString: process.env.DATABASE_URL,
+});
+
+// Créer le client Prisma avec l'adaptateur Neon
+const prisma = new PrismaClient({
+	adapter,
+	log: ['query', 'info', 'warn', 'error'],
+});
+
+export default prisma;
